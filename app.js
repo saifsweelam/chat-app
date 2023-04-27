bae5b002907f9282f4cd34d77d9316c4de0b61f9
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-04-28 02:14:20 +0300

    Add basic dependencies and configuration

---
+++ app.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const express = require('express');
+const nunjucks = require('nunjucks');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const config = require('./config');
+
+// Initialize App and Web Server
+const app = express();
+const server = http.createServer(app);
+
+// Template Engine
+nunjucks.configure(config.views.dir, {
+    express: app,
+    autoescape: true
+});
+app.set('views', config.views.dir);
+app.set('view engine', config.views.engine);
+
+// Static Assets
+app.use(express.static(config.static.dir));
+
+// Handle Form Body
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(config.session));
+app.use(({ session }, res, next) => {
+    res.locals.session = session; // Make session accessible in templates
+    next();
+})
+
+// Flash Messages
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.flashed = {
+        errors: req.flash('error'),
+        warnings: req.flash('warning'),
+        successes: req.flash('success'),
+    };
+    next();
+});
+
+// Run Server
+server.listen(
+    config.deploy.port,
+    () => console.log(`Server running on port ${config.deploy.port}`)
+)
